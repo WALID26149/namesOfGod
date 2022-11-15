20b02f49c5807cc860d5c3fe74a6e1f9520f1d44
walid allane <walid.allane@outlook>
2022-11-15 17:20:10 +0100

    first commit to this project and he is already done

---
+++ index.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('.btn');
+const headTable = document.querySelector('thead');
+
+// make the btn scrolle to the top
+btn.addEventListener('click', function (e) {
+  const s1coords = headTable.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
+
+// lazy load for the page
+const page = document.querySelectorAll('.container');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section-hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+page.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section-hidden');
+});
